6cd6b16f4990b677eecab8cf7d701c24828d79b1
Ngai Tung Kwok <ngai_tung_kwok@aiap.sg>
2022-01-20 16:41:17 +0800

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This mini project attempts to test out the Human-Pose Estimation and Object Detection features in PeekingDuck library',
+    author='eric_kwok',
+    license='',
+)
