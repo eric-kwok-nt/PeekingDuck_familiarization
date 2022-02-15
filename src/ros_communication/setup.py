edeca35ccbdabf61bbad4b09a7162323cddda9e4
Ngai Tung Kwok <ngaitung.kwok@u.nus.edu>
2022-02-15 18:23:52 +0800

    tested simple ROS talker node

---
+++ src/ros_communication/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+package_name = "ros_communication"
+
+setup(
+    name=package_name,
+    version="0.0.0",
+    packages=[package_name],
+    data_files=[
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
+    ],
+    install_requires=["setuptools"],
+    zip_safe=True,
+    maintainer="chamois",
+    maintainer_email="ngaitung.kwok@u.nus.edu",
+    description="ROS communication with PeekingDuck implementation",
+    license="TODO: License declaration",
+    tests_require=["pytest"],
+    entry_points={
+        "console_scripts": ["talker = py_pubsub.publisher_member_function:main"],
+    },
+)
